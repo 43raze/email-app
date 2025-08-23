253a6de5a088fd0fd17b5c3f993f7bbf4cbe97e3
Serhii Hul <owna.sgul@gmail.com>
2025-08-23 17:08:24 +0300

    Style: add template

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,56 @@
 <script></script>
 
 <template>
-  <BAccordion>
-    <BAccordionItem title="Accordion Item #1" visible>
-      <strong>This is the first item's accordion body.</strong> It is shown by
-      default, until the collapse plugin adds the appropriate classes that we
-      use to style each element. These classes control the overall appearance,
-      as well as the showing and hiding via CSS transitions. You can modify any
-      of this with custom CSS or overriding our default variables. It is also
-      worth noting that just about any HTML can go within the
-      <code>.accordion-body</code>, though the transition does limit overflow.
-    </BAccordionItem>
-    <BAccordionItem title="Accordion Item #2">
-      <strong>This is the second item's accordion body.</strong> It is hidden by
-      default, until the collapse plugin adds the appropriate classes that we
-      use to style each element. These classes control the overall appearance,
-      as well as the showing and hiding via CSS transitions. You can modify any
-      of this with custom CSS or overriding our default variables. It is also
-      worth noting that just about any HTML can go within the
-      <code>.accordion-body</code>, though the transition does limit overflow.
-    </BAccordionItem>
-    <BAccordionItem title="Accordion Item #3">
-      <strong>This is the third item's accordion body.</strong> It is hidden by
-      default, until the collapse plugin adds the appropriate classes that we
-      use to style each element. These classes control the overall appearance,
-      as well as the showing and hiding via CSS transitions. You can modify any
-      of this with custom CSS or overriding our default variables. It is also
-      worth noting that just about any HTML can go within the
-      <code>.accordion-body</code>, though the transition does limit overflow.
-    </BAccordionItem>
-  </BAccordion>
+  <BContainer fluid class="vh-100 d-flex flex-column">
+    <BNavbar type="dark" variant="primary">
+      <BNavbarBrand href="#">📧 MyOutlook</BNavbarBrand>
+    </BNavbar>
 
-  <BButton>Button</BButton>
-  <BButton variant="danger">Button</BButton>
-  <BButton variant="success">Button</BButton>
-  <BButton variant="outline-primary">Button</BButton>
+    <!-- Основная часть -->
+    <BRow class="flex-grow-1">
+      <!-- Левая панель -->
+      <BCol cols="2" class="border-end bg-light">
+        <div class="p-2">
+          <BButton variant="primary" block class="mb-3"
+            >✉️ Новое письмо</BButton
+          >
+          <BListGroup>
+            <BListGroupItem active>Входящие</BListGroupItem>
+            <BListGroupItem>Отправленные</BListGroupItem>
+            <BListGroupItem>Черновики</BListGroupItem>
+            <BListGroupItem>Удалённые</BListGroupItem>
+          </BListGroup>
+        </div>
+      </BCol>
+
+      <BCol cols="3" class="border-end overflow-auto">
+        <BListGroup flush>
+          <BListGroupItem>
+            <strong>Иван Петров</strong> <br />
+            Тема письма №1
+          </BListGroupItem>
+          <BListGroupItem>
+            <strong>Google</strong> <br />
+            Подтвердите вход
+          </BListGroupItem>
+          <BListGroupItem>
+            <strong>Мария</strong> <br />
+            Встреча завтра
+          </BListGroupItem>
+        </BListGroup>
+      </BCol>
+
+      <BCol cols="7" class="p-3">
+        <BCard>
+          <h5>Тема письма №1</h5>
+          <p><strong>От:</strong> Иван Петров</p>
+          <hr />
+          <p>
+            Привет! Это тестовое письмо в стиле Outlook. Тут можно отобразить
+            текст письма и вложения.
+          </p>
+        </BCard>
+      </BCol>
+    </BRow>
+  </BContainer>
 </template>
