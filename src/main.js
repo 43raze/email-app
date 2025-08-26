33dc6d629ea268bb4ae4779242f0b89eded54bb3
Serhii Hul <owna.sgul@gmail.com>
2025-08-27 00:49:45 +0300

    delete unplugin & add bootstrap-vue-next

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import './assets/index.css'
-
-import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next/plugins/createBootstrap'
 
 const app = createApp(App)
